145415f354e6adea30850599114f9af8e9bc8da9
Yevgeny Omar Siregar <48908985+yevgenysiregar@users.noreply.github.com>
2022-12-21 12:01:45 +0700

    HCI_2_ClassActivity submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Add your answer here
+$(function(){
+    $(".text-contents").hide();
+
+    $(".lesson").mouseenter(function () {
+        $(this).find('.text-contents').show()
+    });
+
+    $(".lesson").mouseleave(function () {
+        $(this).find('.text-contents').hide()
+    });
+
+});
